036171dbc06b5e66c7ec47f3c4e71028d3496a3c
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-05-08 12:42:05 +0900

    basic example

---
+++ Ex-basic-drl-7.36/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,22 @@
+package com.sample;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsTest {
+
+    public static void main(String[] args) {
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kcontainer = ks.getKieClasspathContainer();
+        KieBase kbase = kcontainer.getKieBase();
+        KieSession ksession = kbase.newKieSession();
+
+        ksession.insert(new Person("John", 35));
+        ksession.fireAllRules();
+        ksession.dispose();
+
+    }
+}
