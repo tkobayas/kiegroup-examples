492047748651deeb46aaef574fe875afbc8cc47c
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-11-16 15:38:33 +0900

    7.46 example with ReteDumper

---
+++ Ex-basic-drl-7.46/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,27 @@
+package com.sample;
+
+import org.drools.core.reteoo.ReteDumper;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsTest {
+
+    public static void main(String[] args) {
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kcontainer = ks.getKieClasspathContainer();
+        KieBase kbase = kcontainer.getKieBase();
+        KieSession ksession = kbase.newKieSession();
+
+        ReteDumper.dumpRete(ksession);
+        
+        ksession.insert(new Person("John", 35));
+        ksession.insert(new Person("Paul", 33));
+
+        ksession.fireAllRules();
+        ksession.dispose();
+
+    }
+}
