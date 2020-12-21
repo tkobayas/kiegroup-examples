dc8280c2e749158c75a9afd7f438739904f83d5e
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2020-12-21 13:13:01 +0900

    agenda clear

---
+++ Ex-agenda-group-7.46/src/main/java/com/sample/AgendaStackClearTest.java
@@ -0,0 +1,35 @@
+package com.sample;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class AgendaStackClearTest {
+
+    public static void main(String[] args) {
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kcontainer = ks.getKieClasspathContainer();
+        KieBase kbase = kcontainer.getKieBase();
+        KieSession ksession = kbase.newKieSession();
+
+        ksession.insert(new Person("John", 35));
+        ksession.insert(new Person("Paul", 33));
+
+        System.out.println("--- calling setFocus() for agenda1");
+        ksession.getAgenda().getAgendaGroup("agenda1").setFocus();
+
+        System.out.println("--- calling setFocus() for agenda2");
+        ksession.getAgenda().getAgendaGroup("agenda2").setFocus();
+
+        System.out.println("--- calling clear() for agenda2");
+        ksession.getAgenda().getAgendaGroup("agenda2").clear();
+
+        System.out.println("--- calling fireAllRules()");
+        ksession.fireAllRules();
+
+        ksession.dispose();
+
+    }
+}
