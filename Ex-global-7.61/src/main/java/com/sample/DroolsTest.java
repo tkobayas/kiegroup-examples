7751801a44052f772bd16e8b13539a30b56b0ea7
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2021-11-05 19:05:46 +0900

    7.61 global

---
+++ Ex-global-7.61/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,33 @@
+package com.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsTest {
+
+    public static void main(String[] args) {
+
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kcontainer = ks.getKieClasspathContainer();
+        KieBase kbase = kcontainer.getKieBase();
+        KieSession ksession = kbase.newKieSession();
+
+        List<String> resultList = new ArrayList<>();
+        ksession.setGlobal("resultList", resultList);
+
+        ksession.insert(new Person("John", 35));
+        ksession.insert(new Person("Paul", 32));
+
+        ksession.fireAllRules();
+
+        System.out.println("resultList = " + resultList);
+
+        ksession.dispose();
+
+    }
+}
