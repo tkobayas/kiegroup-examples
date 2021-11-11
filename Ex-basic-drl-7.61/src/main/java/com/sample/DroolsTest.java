f695dc22bbbd3010dbb8e8840054c7154bbf5a73
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2021-11-11 18:21:28 +0900

    slim down

--- Ex-basic-drl-7.61/src/main/java/com/sample/DroolsTest.java
+++ Ex-basic-drl-7.61/src/main/java/com/sample/DroolsTest.java
@@ -1,6 +1,5 @@
 package com.sample;
 
-import org.drools.core.reteoo.ReteDumper;
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
@@ -10,20 +9,15 @@ public class DroolsTest {
 
     public static void main(String[] args) {
 
-//        System.setProperty("drools.dump.dir", "/home/tkobayas/tmp");
-        
         KieServices ks = KieServices.Factory.get();
         KieContainer kcontainer = ks.getKieClasspathContainer();
         KieBase kbase = kcontainer.getKieBase();
         KieSession ksession = kbase.newKieSession();
 
-//        ReteDumper.dumpRete(ksession);
-        
         ksession.insert(new Person("John", 35));
-        ksession.insert(new Person("Paul", 33));
+        ksession.insert(new Person("Paul", 18));
 
         ksession.fireAllRules();
         ksession.dispose();
-
     }
 }
