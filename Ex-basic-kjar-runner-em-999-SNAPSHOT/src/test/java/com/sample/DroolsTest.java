f77c22f588b6fcb370274aba73fb21263fd404f0
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2024-05-17 10:38:41 +0900

    999-SNAPSHOT exec-model example

---
+++ Ex-basic-kjar-runner-em-999-SNAPSHOT/src/test/java/com/sample/DroolsTest.java
@@ -0,0 +1,26 @@
+package com.sample;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.builder.ReleaseId;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsTest {
+
+    @Test
+    public void testKjar() {
+        KieServices ks = KieServices.Factory.get();
+        ReleaseId releaseId = ks.newReleaseId("com.sample", "basic-kjar-em", "1.0.0");
+        KieContainer kContainer = ks.newKieContainer(releaseId);
+        KieSession kSession = kContainer.newKieSession();
+
+        try {
+            Person john = new Person("john", 25);
+            kSession.insert(john);
+            kSession.fireAllRules();
+        } finally {
+            kSession.dispose();
+        }
+    }
+}
